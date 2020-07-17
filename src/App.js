dd491fde35191c64841cadd25e34a2fb7725942a
Vitaly <vitalgolob@gmail.com>
2020-07-17 22:11:18 +0300

    change function on class

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
-import Button from "./Button";
-import Display from "./Display";
 
-function App() {
-  const [count, setCount] = useState(0);
-  const incrementCount = () => setCount(count + 1);
-  return (
-    <div className="App">
-      <h1>The GitHub Cards</h1>
-      <Button onClickFunction={incrementCount} />
-      <Display message={count} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.props.title}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
